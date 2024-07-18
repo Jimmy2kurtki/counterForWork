6dc3cd685230e38a662aec521f2b17fc0b8bfa88
Jimmy2kurtki <v4vkurse@gmail.com>
2024-07-18 20:21:56 +0800

    sdd

--- app/src/main/java/com/Lessons/lessonsapp/MainActivity.java
+++ app/src/main/java/com/Lessons/lessonsapp/MainActivity.java
@@ -1,27 +1,30 @@
 package com.Lessons.lessonsapp;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private TextView easySim, middleSim, hardSim;
     private Button easy, middle, hard, allSim;
     int intEasySim, intMiddleSim, intHardSim;
-    String[] arraySim = new String[48];
+    public String[] arraySim = new String[50];
     int count = 0;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,16 +33,13 @@ public class MainActivity extends AppCompatActivity {
         easySim = findViewById(R.id.easySim);
         middleSim = findViewById(R.id.middleSim);
         hardSim = findViewById(R.id.hardSim);
-        allSim = findViewById(R.id.allSim);
+        allSim = findViewById(R.id.button_allSim);
         easy = findViewById(R.id.easy);
         middle = findViewById(R.id.middle);
         hard = findViewById(R.id.hard);
-
-
-
     }
 
-    public void setNumberSim(Button btn, EditText in) {
+    public void setCountSim(Button btn) {
         if (btn.equals(easy)) {
             intEasySim++;
             easySim.setText(String.valueOf(intEasySim));
@@ -51,44 +51,38 @@ public class MainActivity extends AppCompatActivity {
             hardSim.setText(String.valueOf(intHardSim));
         }
         allSim.setText(String.valueOf(intEasySim+intMiddleSim+intHardSim));
-        arraySim[count] = String.valueOf(in.getText());
-        count++;
     }
 
-    public String[] getArraySim(){
-        return this.arraySim;
-    }
-
-    public void nextActivity(View v) {
-        Intent intent = new Intent(this,allSimActivity.class);
-        startActivity(intent);
-    }
 
     public void btnClick(View v) {
-        numberSimAlert(((Button) v).getText().toString(), ((Button) v));
+        customDialog(((Button)v).getText().toString(), ((Button)v));
     }
 
-    private void numberSimAlert(String text, Button btn) {
+    private void customDialog(String text,Button btn){
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle(text)
-                .setMessage("Какой номер сэмика?")
+        builder.setTitle("ВВЕДИ НОМЕР СЭМИКА")
+                .setMessage(text)
                 .setCancelable(false);
         EditText input = new EditText(this);
         builder.setView(input);
-        builder.setPositiveButton("этот", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                setNumberSim(btn, input);
-            }
-        });
-        builder.setNegativeButton("НАЗАД", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
+        String numberSim = input.getText().toString();
+        if (numberSim.isEmpty()) numberSim = "+";
+        String finalNumberSim = numberSim;
+        builder.setPositiveButton("ГОТОВО", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        arraySim[count] = finalNumberSim;
+                        count++;
+                        setCountSim(btn);
+                    }
+                })
+                .setNeutralButton("НАЗАД", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        builder.show();
 
     }
 }
